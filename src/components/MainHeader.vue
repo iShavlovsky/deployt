f9fab6dafc17510e0949b734e9712ef101c8a72d
Stanislav V <5777227@gmail.com>
2023-03-29 13:43:38 +0300

    Добавил файлов стилей

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -2,8 +2,7 @@
   <div class='example'>
     <h1> Привет </h1>
   </div>
-  <div class='header'
-       id="header">
+  <div class='header'>
     <h2> Vite </h2>
   </div>
 </template>
@@ -14,15 +13,16 @@ export default {
 };
 </script>
 <style lang="scss">
-.example {
-  display: grid;
-  transition: all .5s;
-  user-select: none;
-  background: linear-gradient(to bottom, white, black);
+.header {
+  margin-top: em(16);
+  display: flex;
+  color: $primary-black;
 }
 
-.header {
-  display: flex;
-  color: $primary-color;
+@include mobile {
+  .header {
+    margin-top: px(16);
+    color: $accent-green;
+  }
 }
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <div class="container">\
+    <div class="container">
       <MainHeader/>
     </div>
   </section>
