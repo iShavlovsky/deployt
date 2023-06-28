b96821f29392b4f08aff1bfaf0eaddc6b6b52b53
Igor Shavlovsky <shahmanul@34gmail.com>
2023-06-28 16:09:23 +0300

    update project 7
    (cherry picked from commit e0e23245673be31a67a9fd0891b9cf9af8264c47)

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -71,7 +71,7 @@
                            :heading="dd.heading"
                            :description="dd.description"
                            :index="index"
-                           :initialOpenIndex="2"/>
+                           :initialOpenIndex="0"/>
             </div>
           </div>
         </div>
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {fileURLToPath, URL} from 'node:url'
 import autoprefixer from 'autoprefixer'
+
 // import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
+
   // build: {
   //   minify: 'terser',
   //   terserOptions: {
